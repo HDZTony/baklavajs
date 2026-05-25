46a7f27fef6e7014204b94c9e9f0296aea62a1cb
hedongzhou <954504788@qq.com>
2026-05-25 14:04:38 +0800

    chore: Remove unused node interface components and styles
    - Deleted ButtonInterface, CheckboxInterface, IntegerInterface, NumberInterface, SelectInterface, SliderInterface, TextInterface, TextareaInputInterface, and TextInputInterface along with their corresponding Vue components and styles.
    - Cleaned up related SCSS files for buttons, checkboxes, inputs, and other UI elements to streamline the codebase.

--- packages/renderer-vue/src/nodeinterfaces/number/NumberInterface.ts
+++
@@ -1,9 +0,0 @@
-import { type ComponentOptions, markRaw } from "vue";
-import { BaseNumericInterface } from "../baseNumericInterface";
-import NumberInterfaceComponent from "./NumberInterface.vue";
-
-export class NumberInterface extends BaseNumericInterface {
-    component = markRaw(NumberInterfaceComponent) as ComponentOptions;
-}
-
-export { NumberInterfaceComponent };
--- packages/renderer-vue/src/nodeinterfaces/textareainput/TextareaInputInterface.ts
+++
@@ -1,9 +0,0 @@
-import { type ComponentOptions, markRaw } from "vue";
-import { NodeInterface } from "@baklavajs/core";
-import TextareaInputInterfaceComponent from "./TextareaInputInterface.vue";
-
-export class TextareaInputInterface extends NodeInterface<string> {
-    component = markRaw(TextareaInputInterfaceComponent) as ComponentOptions;
-}
-
-export { TextareaInputInterfaceComponent };
